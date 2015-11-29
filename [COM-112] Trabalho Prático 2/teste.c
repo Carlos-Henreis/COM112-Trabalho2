5b4d54a896f74e1f44faf4a5e479451fc20f666d
Carlos-Henreis <carlos_henreis@unifei.edu.br>
2015-11-29 20:08:31 -0200

    Projeto adicionado

---
+++ [COM-112] Trabalho Prático 2/teste.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   teste.c
+ * Author: carlos
+ *
+ * Created on 29 de Novembro de 2015, 20:07
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
